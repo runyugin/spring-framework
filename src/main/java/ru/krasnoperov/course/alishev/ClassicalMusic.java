1adb8338cf8195e639963bbc5bf5818dbeb99271
krasnoperov <runyugin@mail.ru>
2022-12-13 16:22:21 +0300

    lesson5

--- src/main/java/ru/krasnoperov/course/alishev/ClassicalMusic.java
+++ src/main/java/ru/krasnoperov/course/alishev/ClassicalMusic.java
@@ -1,8 +1,23 @@
 package ru.krasnoperov.course.alishev;
 
 public class ClassicalMusic implements Music{
+
+    public void doMyInit(){
+        System.out.println("Start doMyInit");
+    }
+    public void doMyDestropy(){
+        System.out.println("Start doMyDestropy");
+    }
+
+    private ClassicalMusic() {
+    }
+
+    public static ClassicalMusic getNewClassicalMusic(){
+        return new ClassicalMusic();
+    }
+
     @Override
     public String getSong() {
-        return "Sonata #5";
+        return "Sonata #";
     }
 }
--- src/main/java/ru/krasnoperov/course/alishev/RockMusic.java
+++ src/main/java/ru/krasnoperov/course/alishev/RockMusic.java
@@ -5,4 +5,10 @@ public class RockMusic implements Music{
     public String getSong() {
         return "Перемен";
     }
+    public void doMyInit(){
+        System.out.println("Start doMyInit");
+    }
+    public void doMyDestropy(){
+        System.out.println("Start doMyDestropy");
+    }
 }
